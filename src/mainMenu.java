955ea690e9e159965affcc957f92ba8cac9e8368
Abel Gonzalez <60588691+agonzalez218@users.noreply.github.com>
2023-03-13 17:34:15 -0500

    Added Number Select and TestData List File

--- src/mainMenu.java
+++ src/mainMenu.java
@@ -1,12 +1,17 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class mainMenu extends JFrame implements ActionListener {
-    public List<Integer> testData = new ArrayList<>();
+    List<Integer> testData = new ArrayList<>();
     JPanel form = new JPanel();
     JButton changeTestDataBttn = new JButton();
     JLabel currentDataLbl = new JLabel();
@@ -34,11 +39,6 @@ public class mainMenu extends JFrame implements ActionListener {
     private void setText(){
         this.setTitle("Java Interactive Structures");
         currentDataLbl.setText("Current Data:");
-        int i;
-        for(i = 0; i < 20; i++)
-        {
-            testData.add(ThreadLocalRandom.current().nextInt(0, 1000 + 1));
-        }
         currentDataTxt.setText(testData.toString().replace("[", "").replace("]", ""));
         changeTestDataBttn.setText("Change Test Data");
         viewStructuresBttn.setText("View Data Structures");
@@ -66,6 +66,7 @@ public class mainMenu extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.form.setLayout(null);
         setLocations();
+        getTestDataFile();
         setText();
         addToForm();
         this.setVisible(true);
@@ -73,8 +74,20 @@ public class mainMenu extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         String str = e.getActionCommand();
 
+        setTestDataFile();
+
         switch (str) {
-            case "Exit Application" -> System.exit(0);
+            case "Exit Application" -> {
+                try {
+                    FileWriter myWriter = new FileWriter("testData.txt");
+                    myWriter.write("");
+                    myWriter.close();
+                } catch (IOException ex) {
+                    System.out.println("Error in Number Array Select finding file, creating new one...");
+                    ex.printStackTrace();
+                }
+                System.exit(0);
+            }
             case "Change Test Data" -> {
                 numberArraySelect numWindow = new numberArraySelect();
                 this.setVisible(false);
@@ -89,6 +102,64 @@ public class mainMenu extends JFrame implements ActionListener {
                 displayAlgorithms algorithmsWindow = new displayAlgorithms();
                 this.setVisible(false);
                 algorithmsWindow.setVisible(true);
+            }
+        }
+    }
+
+    /*
+      Description:
+          Opens testData.txt file and enters data from TestData list
+    */
+    public void setTestDataFile() {
+        try {
+            int i;
+            FileWriter myWriter = new FileWriter("testData.txt");
+            for (i = 0; i < testData.size(); i++) {
+                myWriter.write(testData.get(i).toString());
+                myWriter.write("\n");
+            }
+            myWriter.close();
+        } catch (IOException ex) {
+            System.out.println("Error in Number Array Select finding file");
+            ex.printStackTrace();
+        }
+    }
+
+    /*
+      Description:
+          Opens testData.txt file and adds all integers into the TestData list.
+          If file not found, fill TestData list with random numbers and create file.
+    */
+    public void getTestDataFile() {
+        try {
+            File myObj = new File("testData.txt");
+            Scanner myReader = new Scanner(myObj);
+            int counter = 0;
+            while (myReader.hasNextLine()) {
+                counter += 1;
+                try {
+                    testData.add(Integer.valueOf(myReader.nextLine()));
+                }
+                catch(NumberFormatException n){
+                    System.out.println("Line " + counter + " in file did not contain integer");
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error in Main Menu finding file, creating new one...");
+            int i;
+            try {
+                FileWriter myWriter = new FileWriter("testData.txt");
+                for(i = 0; i < 20; i++)
+                {
+                    testData.add(ThreadLocalRandom.current().nextInt(0, 1000 + 1));
+                    myWriter.write(testData.get(i).toString());
+                    myWriter.write("\n");
+                }
+                myWriter.close();
+            } catch (IOException ex) {
+                System.out.println("An error occurred.");
+                ex.printStackTrace();
             }
         }
     }
--- src/numberArraySelect.java
+++ src/numberArraySelect.java
@@ -1,10 +1,181 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class numberArraySelect extends JFrame implements ActionListener {
+    List<Integer> newTestData = new ArrayList<>();
+
+    boolean arrayChanged = false;
+    JPanel form = new JPanel();
+    JButton mainMenuBttn = new JButton();
+    JButton fillRdmBttn = new JButton();
+    JButton[] dataPoint = new JButton[8];
+    JButton textBoxBttn = new JButton();
+
+    private void setLocations(){
+        this.setSize(600, 350);
+        this.setLocationRelativeTo(null);
+        mainMenuBttn.setBounds(30, 260, 150, 30);
+        fillRdmBttn.setBounds(190, 260, 200, 30);
+        textBoxBttn.setBounds(400, 260, 150, 30);
+    }
+
+    private void setText(){
+        this.setTitle("Change Test Data");
+        mainMenuBttn.setText("Return to Main Menu");
+        fillRdmBttn.setText("Fill with Random Nums");
+        textBoxBttn.setText("Dump Test Data");
+    }
+
+    private void addToForm(){
+        this.add( mainMenuBttn );
+        mainMenuBttn.addActionListener(this);
+        this.add( textBoxBttn );
+        textBoxBttn.addActionListener(this);
+        this.add( fillRdmBttn );
+        fillRdmBttn.addActionListener(this);
+    }
+
+
+    public numberArraySelect(){
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.form.setLayout(null);
+        setLocations();
+        setText();
+        addToForm();
+
+        int i;
+        for( i = 0; i < 8; i++ )
+        {
+            JButton tempSquare = new JButton();
+            if( i < 4 )
+            {
+                tempSquare.setBounds(30+(i*150),25,75,75);
+            }
+            else {
+                tempSquare.setBounds(30+((i-4)*150),150,75,75);
+            }
+            tempSquare.addActionListener(this);
+            dataPoint[i] = tempSquare;
+            this.add(dataPoint[i]);
+        }
+        changeDataPoints();
+        this.add(this.form);
+    }
+
+    /*
+      Description:
+          Changes all buttons' number choice and re - randomizes it.
+    */
+    public void changeDataPoints()
+    {
+        int i;
+        for(i = 0; i < 8; i++)
+        {
+            dataPoint[i].setText(String.valueOf(ThreadLocalRandom.current().nextInt(0, 1000 + 1)));
+        }
+    }
+
+    /*
+      Description:
+          Opens testData.txt file and enters data from TestData list
+    */
+    public void setTestDataFile() {
+        try {
+            int i;
+            FileWriter myWriter = new FileWriter("testData.txt");
+            for (i = 0; i < newTestData.size(); i++) {
+                myWriter.write(newTestData.get(i).toString());
+                myWriter.write("\n");
+            }
+            myWriter.close();
+        } catch (IOException ex) {
+            System.out.println("Error in Number Array Select finding file");
+            ex.printStackTrace();
+        }
+    }
+
+    /*
+      Description:
+          Opens testData.txt file and adds all integers into the TestData list.
+          If file not found, fill TestData list with random numbers and create file.
+    */
+    public void getTestDataFile() {
+        try {
+            File myObj = new File("testData.txt");
+            Scanner myReader = new Scanner(myObj);
+            int counter = 0;
+            while (myReader.hasNextLine()) {
+                counter += 1;
+                try {
+                    newTestData.add(Integer.valueOf(myReader.nextLine()));
+                }
+                catch(NumberFormatException n){
+                    System.out.println("Line " + counter + " in file did not contain integer");
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error in Main Menu finding file, creating new one...");
+            int i;
+            try {
+                FileWriter myWriter = new FileWriter("testData.txt");
+                for(i = 0; i < 20; i++)
+                {
+                    newTestData.add(ThreadLocalRandom.current().nextInt(0, 1000 + 1));
+                    myWriter.write(newTestData.get(i).toString());
+                    myWriter.write("\n");
+                }
+                myWriter.close();
+            } catch (IOException ex) {
+                System.out.println("An error occurred.");
+                ex.printStackTrace();
+            }
+        }
+    }
 
     public void actionPerformed(ActionEvent e) {
+        String str = e.getActionCommand();
+        switch (str) {
+            case "Return to Main Menu" -> {
+                if( arrayChanged ) {
+                    setTestDataFile();
+                }
 
+                mainMenu menu = new mainMenu();
+                this.setVisible(false);
+                menu.setVisible(true);
+            }
+            case "Fill with Random Nums" -> {
+                int i;
+                try {
+                    FileWriter myWriter = new FileWriter("testData.txt");
+                    for(i = 0; i < 20; i++)
+                    {
+                        newTestData.add(ThreadLocalRandom.current().nextInt(0, 1000 + 1));
+                        myWriter.write(newTestData.get(i).toString());
+                        myWriter.write("\n");
+                    }
+                    myWriter.close();
+                } catch (IOException ex) {
+                    System.out.println("An error occurred.");
+                    ex.printStackTrace();
+                }
+            }
+            case "Dump Test Data" -> getTestDataFile();
+            default -> {
+                arrayChanged = true;
+                newTestData.add(Integer.valueOf(str));
+                changeDataPoints();
+            }
+        }
     }
 }
